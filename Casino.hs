d3e271f47f2ca40f89d76ecec839532ffeb82c40
Chung-chieh Shan <ccshan@indiana.edu>
2019-08-05 11:29:39 +0100

    Validate casino inference

--- Casino.hs
+++ Casino.hs
@@ -1,8 +1,11 @@
 {-# OPTIONS -Wall #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
 module Casino where
 
 import Dist
 import Plot (renderToFile, plotHeatMap, plotHistogram, tally, binPair, binFloat, binIx)
+import Graphics.Rendering.Chart (layout_plots, layout_x_axis, laxis_generate, scaledAxis)
+import Control.Lens ((.~))
 import Data.Default (Default (def))
 import Data.Number.LogFloat (pow)
 import Control.Monad (replicateM)
@@ -29,6 +32,16 @@ casino' = do
 main :: IO ()
 main = do
   samples <- tabSample 10000 casino'
-  -- let values = tally (binPair (binFloat 20) binIx) samples
-  -- print (dumpDoc values)
   renderToFile def "/tmp/plot.png" (plotHeatMap def (binFloat 20, binIx) samples)
+
+validate :: IO ()
+validate = do
+  estimates <- replicateM 200 $ do
+    samples <- tabSample 1000 (fmap ((> 0) . snd) casino)
+    let [((_,False,_),wf), ((_,True ,_),wt)] = tally binIx samples
+    print (dumpDoc (wf, wt))
+    return (fromProb wt / (fromProb wt + fromProb wf), 1::Int)
+  let lay plots = layout_plots .~ plots
+                $ (layout_x_axis . laxis_generate) .~ scaledAxis def (0.8,1)
+                $ def
+  renderToFile def "/tmp/plot.png" $ lay $ plotHistogram (binFloat 20) estimates
